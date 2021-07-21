d045dd2fd2d9989011c2bcc35af2ee9925530b2c
StephenLungHongKong <52762103+StephenLungHongKong@users.noreply.github.com>
2021-07-21 19:22:15 +1200

    adjust view angle

--- portfolio/scripts/app.js
+++ portfolio/scripts/app.js
@@ -58,8 +58,8 @@ if (mobileAndTabletCheck()) {
 
   //Moving Animation Event
   addEventListener("deviceorientation", (e) => {
-    let xAxis = 15 - e.gamma/ 5 % 30 ;
-    let yAxis = 20 - e.beta / 5 % 40;
+    let xAxis = (e.gamma / 10 - 30) % 30;
+    let yAxis = (e.beta / 10 - 15) % 40;
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
     myname.innerHTML = String(Math.round(xAxis)) + ":" + String(Math.round(yAxis));
   
